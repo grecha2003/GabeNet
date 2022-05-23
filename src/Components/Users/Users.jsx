b9a29c5980ff2e9b8ebc1f79a360c1b680c54b15
Eredin <davidtamliani@yandex.ru>
2022-05-23 21:44:53 +0300

    update

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,88 +1,80 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import avaUsers from "../../Components/assets/avaUsers.png";
-import classes from "./Users.module.css";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import avaUsers from '../../Components/assets/avaUsers.png';
+import classes from './Users.module.css';
 
 const Users = (props) => {
-  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
-  let pages = [];
-  for (let i = 1; i <= pagesCount; i++) {
-    pages.push(i);
-    if (i === 8) break;
-  }
-
-  return (
-    <div>
-      <div>
-        {pages.map((p) => {
-          return (
-            <span
-              onClick={() => {
-                props.onPageChanged(p);
-              }}
-              className={
-                props.currentPage === p
-                  ? classes.selectedPage
-                  : props.currentPage
-              }
-            >
-              {p}
-            </span>
-          );
-        })}
-      </div>
-      {props.users.map((user) => (
-        <div key={user.id}>
-          <span>
-            <div>
-              <NavLink to={"/profile/" + user.id}>
-                <img
-                  src={user.photos.small != null ? user.photos.small : avaUsers}
-                  className={classes.avatarImg}
-                  alt="user_avatar"
-                />
-              </NavLink>
-            </div>
-            <div>
-              {user.followed ? (
-                <button
-                  disabled={props.followingInProgress.some(
-                    (id) => id === user.id
-                  )}
-                  onClick={() => {
-                    props.unfollow(user.id);
-                  }}
-                >
-                  Unfollow
-                </button>
-              ) : (
-                <button
-                  disabled={props.followingInProgress.some(
-                    (id) => id === user.id
-                  )}
-                  onClick={() => {
-                    props.follow(user.id);
-                  }}
-                >
-                  Follow
-                </button>
-              )}
-            </div>
-          </span>
-          <span>
-            <span>
-              <div>{user.name}</div>
-              <div>{user.status}</div>
-            </span>
-            <span>
-              <div>{"users.location.city"}</div>
-              <div>{"users.location.state"}</div>
-            </span>
-          </span>
-        </div>
-      ))}
-    </div>
-  );
+	let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+	let pages = [];
+	for (let i = 1; i <= pagesCount; i++) {
+		pages.push(i);
+		if (i === 8) break;
+	}
+	return (
+		<div>
+			<div>
+				{pages.map((p) => {
+					return (
+						<span
+							onClick={() => {
+								props.onPageChanged(p);
+								debugger;
+							}}
+							className={props.currentPage === p ? classes.selectedPage : props.currentPage}
+						>
+							{p}
+						</span>
+					);
+				})}
+			</div>
+			{props.users.map((user) => (
+				<div key={user.id}>
+					<span>
+						<div>
+							<NavLink to={'/profile/' + user.id}>
+								<img
+									src={user.photos.small != null ? user.photos.small : avaUsers}
+									className={classes.avatarImg}
+									alt="user_avatar"
+								/>
+							</NavLink>
+						</div>
+						<div>
+							{user.followed ? (
+								<button
+									disabled={props.followingInProgress.some((id) => id === user.id)}
+									onClick={() => {
+										props.unfollow(user.id);
+									}}
+								>
+									Unfollow
+								</button>
+							) : (
+								<button
+									disabled={props.followingInProgress.some((id) => id === user.id)}
+									onClick={() => {
+										props.follow(user.id);
+									}}
+								>
+									Follow
+								</button>
+							)}
+						</div>
+					</span>
+					<span>
+						<span>
+							<div>{user.name}</div>
+							<div>{user.status}</div>
+						</span>
+						<span>
+							<div>{'users.location.city'}</div>
+							<div>{'users.location.state'}</div>
+						</span>
+					</span>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default Users;
