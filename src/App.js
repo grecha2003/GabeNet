78c4d303fc9c6fd8124f1bd0b0999e24750a6233
Eredin <davidtamliani@yandex.ru>
2022-06-06 23:24:59 +0300

    up

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ import UsersContainer from './Components/Users/UsersContainer';
 import ProfileContainer from './Components/Profile/ProfileContainer';
 import HeaderContainer from './Components/Header/HeaderContainer';
 import Login from './Components/Login/Login';
-import { connect } from 'react-redux';
+import { connect, Provider } from 'react-redux';
 import { initializeApp } from './Redux/appReducer';
 import Spinner from './Components/common/Spinner/Spinner';
+import store from './Redux/reduxStore';
 
 class App extends React.Component {
 	componentDidMount() {
@@ -23,21 +24,23 @@ class App extends React.Component {
 		}
 
 		return (
-			<BrowserRouter>
-				<div className="App">
-					<HeaderContainer path="/login" />
-					<Navbar />
-					<div className="App__Content">
-						<Routes>
-							<Route path="/messages" element={<MessagesContainer />} />
-							<Route path="/profile" element={<ProfileContainer />} />
-							<Route path="/profile/:userId" element={<ProfileContainer />} />
-							<Route path="/users" element={<UsersContainer />} />
-							<Route path="/login" element={<Login />} />
-						</Routes>
+			<>
+				<BrowserRouter>
+					<div className="App">
+						<HeaderContainer path="/login" />
+						<Navbar />
+						<div className="App__Content">
+							<Routes>
+								<Route path="/messages" element={<MessagesContainer />} />
+								<Route path="/profile" element={<ProfileContainer />} />
+								<Route path="/profile/:userId" element={<ProfileContainer />} />
+								<Route path="/users" element={<UsersContainer />} />
+								<Route path="/login" element={<Login />} />
+							</Routes>
+						</div>
 					</div>
-				</div>
-			</BrowserRouter>
+				</BrowserRouter>
+			</>
 		);
 	}
 }
@@ -46,4 +49,14 @@ const mapStateToProps = (state) => ({
 	initialized: state.app.initialized,
 });
 
-export default connect(mapStateToProps, { initializeApp })(App);
+let AppContainer = connect(mapStateToProps, { initializeApp })(App);
+
+let AppRenderContainer = () => {
+	return (
+		<Provider store={store}>
+			<AppContainer />
+		</Provider>
+	);
+};
+
+export default AppRenderContainer;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import App from './App';
-import store from './Redux/reduxStore';
+import AppRenderContainer from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-	<Provider store={store}>
-		<App />
-	</Provider>
-);
+root.render(<AppRenderContainer />);
